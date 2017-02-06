364e44a02ed8b7014ec97fe9ead7850fb76cf6aa
GrosOurs88 <hugo.bourbon@wanadoo.fr>
2017-02-07 00:39:59 +0100

    Ajout_scenes_test + script_teleportation

---
+++ Assets/Scripts/AvatarTeleport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AvatarTeleport : MonoBehaviour
+{
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+			{
+				SceneManager.LoadScene("Hugo_salle_test_1");
+			}
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+			{
+				SceneManager.LoadScene("Hugo_salle_test_2");
+			}
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+			{
+				SceneManager.LoadScene("Hugo_salle_test_3");
+			}
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+			{
+				SceneManager.LoadScene("Hugo_salle_test_4");
+			}
+		if (Input.GetKeyDown(KeyCode.Alpha5))
+			{
+				SceneManager.LoadScene("Hugo_salle_test_5");
+			}
+	}
+}
