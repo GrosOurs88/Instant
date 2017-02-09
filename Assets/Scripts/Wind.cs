9cce31284d8979cddfbec484c1197c04175046b4
lmoge <ludovicmoge.ens@gmail.com>
2017-02-09 17:58:40 +0100

    Add wind script

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    public bool active = false;
+    public Vector3 appliedForce = Vector3.forward;
+    public float multiplicator = 3.0f;
+
+    private GameObject[] blocks;
+
+    void Start()
+    {
+        blocks = GameObject.FindGameObjectsWithTag("Block");
+    }
+
+    void Update()
+    {
+        if (active)
+        {
+            foreach(GameObject go in blocks)
+            {
+                go.GetComponent<Rigidbody>().AddForce(appliedForce*multiplicator);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            active = !active;
+        }
+    }
+}
